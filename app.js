f14cb73c6a6967b69142d064b636ddbd761f48ac
blvckjoy <sirjeggz@gmail.com>
2025-04-01 00:35:34 +0100

    Add root route ad fixed Render deployment

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 3000;
 const studentRoutes = require("./routes/studentRoutes");
 
 app.use(express.json());
+
+// Root route for the base URL
+app.get("/", (req, res) => {
+   res.send("Welcome to the Students API");
+});
+
 app.use("/api/students", studentRoutes);
 
 app.listen(port, () => {
